fdda9447441c3fb67c890799408e75804ba6f4da
Sherjil Ozair <sherjilozair@gmail.com>
2017-03-10 03:10:07 -0500

    add image

--- inpainting_mnist.py
+++ inpainting_mnist.py
@@ -21,20 +21,21 @@ class Model:
     def __init__(self):
         self.sess = tf.Session()
         self.inputs = x = tf.placeholder(tf.float32, [None, 28, 28, 1])
-        self.outputs = tf.placeholder(tf.float32, [None, 14, 14, 1])
 
         h = slim.conv2d(x, 128, [1, 1], activation_fn=None, scope='first')
 
         for t in xrange(1):
-            for i, rate in enumerate([1, 2, 1, 2, 1, 2, 1]):
+            for i, rate in enumerate([1, 2, 4, 2, 1, 2, 4, 2, 1]):
                 h = block(h, i, n_filters=64, rate=rate)
 
         y = slim.conv2d(h, 1, [1, 1], activation_fn=None, scope='last')
         self.preds = y[:, 7:21, 7:21, :]
 
+        self.outputs = tf.placeholder(tf.float32, [None, 14, 14, 1])
+
         self.losses = tf.reduce_sum(tf.square(self.outputs - self.preds), axis=[1, 2, 3])
         self.loss = tf.reduce_mean(self.losses)
-        self.train_op = tf.train.AdamOptimizer(1e-5).minimize(self.loss)
+        self.train_op = tf.train.AdamOptimizer(1e-4).minimize(self.loss)
 
         self.sess.run(tf.global_variables_initializer())
         self.saver = tf.train.Saver()
@@ -58,25 +59,23 @@ mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 mbsz = 64
 model = Model()
 
-tlosses = []
-vlosses = []
+losses = []
 
 for i in xrange(10**6):
-    tx = mnist.train.next_batch(mbsz)[0].reshape(-1, 28, 28, 1)
-    loss, _ = model.run(tx)
-    tlosses.append(loss)
+    x = mnist.train.next_batch(mbsz)[0].reshape(-1, 28, 28, 1).copy()
+    loss, _ = model.run(x)
+    losses.append(loss)
+    mloss = np.mean(losses[-1000:])
+    print i, mloss, '\r',
 
-    vx = mnist.validation.next_batch(mbsz)[0].reshape(-1, 28, 28, 1)
-    loss, = model.run(vx, train=False)
-    vlosses.append(loss)
-
-    print i, np.mean(tlosses[-1000:]), np.mean(vlosses[-1000:])#, '\r',
 
     if i % 1000 == 0:
         preds = model.complete(mnist.train.next_batch(mbsz)[0].reshape(-1, 28, 28, 1))
         np.save('/data/lisa/exp/ozairs/predictions/mnist_train_completions_{}.npy'.format(i), preds)
         preds = model.complete(mnist.validation.next_batch(mbsz)[0].reshape(-1, 28, 28, 1))
         np.save('/data/lisa/exp/ozairs/predictions/mnist_valid_completions_{}.npy'.format(i), preds)
+        print 'Saved in /data/lisa/exp/ozairs/predictions/mnist_train_completions_{}.npy'.format(i)
+        print 'Saved in /data/lisa/exp/ozairs/predictions/mnist_valid_completions_{}.npy'.format(i)
+        save_path = model.saver.save(model.sess, '/data/lisa/exp/ozairs/mnist_models/')
+        print "Saved in {}".format(save_path)
 
-    #save_path = model.saver.save(model.sess, '/data/lisa/exp/ozairs/mnist_models/')
-    #print "Saved in {}".format(save_path)
