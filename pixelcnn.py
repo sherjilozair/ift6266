2b8aa674bc9d05c56e61f74beb3c24c97503477c
Sherjil Ozair <sherjilozair@gmail.com>
2017-03-20 18:29:06 -0400

    pixelcnnpp

--- pixelcnn.py
+++ pixelcnn.py
@@ -1,3 +1,4 @@
+import sys, os
 import numpy as np
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
@@ -17,14 +18,14 @@ class nn:
         if mask:
             mid_x = shape[0]/2
             mid_y = shape[1]/2
-            mask_filter = np.ones(shape, dtype=np.float32)
-            mask_filter[mid_x, mid_y+1:, :, :] = 0.
-            mask_filter[mid_x+1:, :, :, :] = 0.
+            mask_filter = np.ones(shape, dtype=np.bool)
+            mask_filter[mid_x, mid_y+1:, :, :] = False
+            mask_filter[mid_x+1:, :, :, :] = False
 
             if mask == 'a':
-                mask_filter[mid_x, mid_y, :, :] = 0.
+                mask_filter[mid_x, mid_y, :, :] = False
 
-            W *= mask_filter
+            W  = W * mask_filter
 
         h = tf.add(tf.nn.conv2d(inputs, W, strides=[1,1,1,1], padding='SAME'), b)
 
@@ -35,22 +36,39 @@ class nn:
 
 
 class PixelCNN:
+    mask = np.ones((64, 64, 1), dtype=np.bool)
+    mask[16:48, 16:48, :] = False
+
     def __init__(self):
-        self.image = tf.placeholder(tf.float32, [None, 28, 28, 1])
-        h = nn.conv2d(self.image, 256, [7, 7], mask='a', scope='conv/7x7')
-        for i in xrange(12):
+        self.image = tf.placeholder(tf.float32, [None, 64, 64, 3])
+        self.outer = self.image * self.mask
+        h = nn.conv2d(self.image, 256, [7, 7], mask='a', activation_fn=None, scope='conv/7x7/ul')
+        j = nn.conv2d(self.outer, 256, [7, 7], activation_fn=None, scope='conv/7x7/ff')
+
+        for i in xrange(8):
             ih = h
-            h = nn.conv2d(h, 128, [1, 1], scope='conv/1x1/%d/1' % i)
-            h = nn.conv2d(h, 128, [3, 3], mask='b', scope='conv/3x3/%d' % i)
-            h = nn.conv2d(h, 256, [1, 1], scope='conv/1x1/%d/2' % i)
-            h = ih + h
+            h = nn.conv2d(h, 128, [1, 1], scope='conv/1x1/%d/1/ul' % i)
+            h = nn.conv2d(h, 128, [5, 5], mask='b', scope='conv/3x3/%d/ul' % i)
+            h = nn.conv2d(h, 256, [1, 1], activation_fn=None, scope='conv/1x1/%d/2/ul' % i)
+            h += ih
+
+            ij = j
+            j = nn.conv2d(j, 128, [1, 1], scope='conv/1x1/%d/1/ff' % i)
+            j = nn.conv2d(j, 128, [5, 5], scope='conv/3x3/%d/ff' % i)
+            j = nn.conv2d(j, 256, [1, 1], scope='conv/1x1/%d/2/ff' % i)
+            j += ij
+
+            h = nn.conv2d(tf.concat([h, j], axis=3), 256, [1, 1], activation_fn=None, scope='conv/concat/%d' % i)
+
         h = nn.conv2d(h, 32, [1, 1], scope='conv/relu/1x1/1')
         h = nn.conv2d(h, 32, [1, 1], scope='conv/relu/1x1/2')
-        self.logits = nn.conv2d(h, 1, [1, 1], activation_fn=None, scope='conv/logits')
-        self.means = tf.nn.sigmoid(self.logits)
+        self.logits = nn.conv2d(h, 3, [1, 1], activation_fn=None, scope='conv/logits')
+
+        self.preds = tf.nn.sigmoid(self.logits)
         self.losses = tf.nn.sigmoid_cross_entropy_with_logits(logits=self.logits, labels=self.image)
+        self.losses = self.losses[:, 16:48, 16:48, :]
         self.loss = tf.reduce_mean(tf.reduce_sum(self.losses, axis=[1, 2, 3]))
-        self.train_op = tf.train.AdamOptimizer().minimize(self.loss)
+        self.train_op = tf.train.AdamOptimizer(1e-5).minimize(self.loss)
         self.sess = tf.Session()
         self.sess.run(tf.global_variables_initializer())
         self.saver = tf.train.Saver()
@@ -60,55 +78,68 @@ class PixelCNN:
 
     def load_params(self, path):
         self.saver.restore(self.sess, path)
+        print "params restored from {}".format(path)
 
-    def train(self, train, valid, mbsz=64, nb_epochs=200):
+    def train(self, name, train, valid, mbsz=32, nb_epochs=200):
+        tidx = np.arange(len(train))
+        vidx = np.arange(len(valid))
+
         for e in xrange(nb_epochs):
             train_losses = []
-            idx = np.arange(len(train))
-            np.random.shuffle(idx)
-            for i in xrange(1000):
-                image = train[idx[mbsz*i:mbsz*(i+1)]]
+            validation_losses = []
+
+            np.random.shuffle(tidx)
+
+            for i in xrange(0, len(tidx)/5, mbsz):
+                image = train[tidx[i:i+mbsz]]
                 _, l = self.sess.run([self.train_op, self.loss], {self.image: image})
                 train_losses.append(l)
                 print 'training...', i, np.mean(train_losses), '\r',
 
-            validation_losses = []
-            idx = np.arange(len(valid))
-            np.random.shuffle(idx)
+            np.random.shuffle(vidx)
 
-            for j in xrange(100):
-                image = valid[idx[mbsz*i:mbsz*(i+1)]]
+            for j in xrange(0, len(vidx)/5, mbsz):
+                image = valid[vidx[i:i+mbsz]]
                 l, = self.sess.run([self.loss], {self.image: image})
                 validation_losses.append(l)
                 print 'validating...', j, np.mean(validation_losses), '\r',
 
-            path = self.save_params("models/ckpt_{}".format(e))
-            #self.sample('samples/sample_{}.png'.format(e))
-            print "epoch: {}/{}, train loss: {}, validation loss: {}, model saved: {}".format(e, nb_epochs, np.mean(train_losses), np.mean(validation_losses), path)
+            path = self.save_params("{}/model".format(name))
+            self.sample('{}/sample_{}.png'.format(name, e), train[tidx[:16]])
+            print "epoch: {}/{}, train loss: {}, validation loss: {}, model saved: {}".format(e,
+                    nb_epochs, np.mean(train_losses), np.mean(validation_losses), path)
 
+            del train_losses
+            del validation_losses
 
-    def sample(self, name, n=8):
-        means = np.zeros((n*n, 28, 28, 1))
-        image = np.zeros((n*n, 28, 28, 1))
-        for i in xrange(28):
-            for j in xrange(28):
-                pixelmean, = self.sess.run([self.means[:, i, j, :]], {self.image: image})
-                means[:, i, j, :] = pixelmean
-                image[:, i, j, :] = np.cast[np.float32](np.random.random(pixelmean.shape) < pixelmean)
+    def sample(self, name, image, n=4):
+        image[:, 16:48, 16:48, :] = 0.
+        for i in xrange(16, 48):
+            for j in xrange(16, 48):
+                pixel, = self.sess.run([self.preds[:, i, j, :]], {self.image: image})
+                image[:, i, j, :] = pixel
                 print 'sampling...', i, j, '\r',
-        canvas = Image.new('L', (32*n, 32*n))
+
+        canvas = Image.new('RGB', (72*n, 72*n))
         for i in xrange(n):
             for j in xrange(n):
-                im = Image.fromarray(np.cast[np.uint8](means[i*n+j, :, :, 0] * 255))
-                canvas.paste(im, (32*i+2, 32*j+2))
+                im = Image.fromarray(np.cast[np.uint8](image[i*n+j, :, :, :] * 255))
+                canvas.paste(im, (72*i+4, 72*j+4))
         canvas.save(name)
 
 
 if __name__ == '__main__':
     model = PixelCNN()
 
+    expname = sys.argv[1]
     datahome = '/data/lisa/exp/ozairs/'
-    train = np.load(datahome + 'images.train.npz').items()[0][1]
-    valid = np.load(datahome + 'images.valid.npz').items()[0][1]
+    train = np.load(datahome + 'images.train.npz').items()[0][1] / 255.
+    valid = np.load(datahome + 'images.valid.npz').items()[0][1] / 255.
 
-    model.train(train, valid)
+    if not os.path.exists(expname):
+        os.makedirs(expname)
+    else:
+        ckpt_file = "{}/model".format(expname)
+        model.load_params(ckpt_file)
+
+    model.train(expname, train, valid)
